b555fb21ae209fd8e7cbfd005bd37b8643103d48
marynahaida <marinkagaida.15@gmail.com>
2024-05-06 16:08:02 +0200

    first commit

---
+++ src/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Main {
+    private static final double CONV_K = 2.20462;
+    public static void main(String[] args) {
+        System.out.println("Converter App");
+
+        double kgs = 5;
+        double pounds = convLgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds");
+    }
+    private static double convLgsToPounds(double kgs){
+        return kgs * CONV_K;
+    }
+}
